abc3a23d5e07fba82e51c2750dd1b8070e73b84c
Marcin Puc <tranzystorek.io@protonmail.com>
2023-08-29 13:05:14 +0200

    Add DEBUG_MODE config variable

--- zellij.plugin.zsh
+++ zellij.plugin.zsh
@@ -1,5 +1,11 @@
 #!/usr/bin/env zsh
 
+_is_config_enabled() {
+    local config_name="$1"
+    local variable_name="ZELLIJ_ZSH_${config_name}"
+    [[ -n "${(P)variable_name}" && "${(P)variable_name}" != "0" && "${(P)variable_name}" != "false" ]]
+}
+
 _is_zellij_cmd_available() {
     [[ -v commands[zellij] ]]
 }
@@ -9,12 +15,17 @@ _is_zellij_running() {
 }
 
 _is_vscode_disabled() {
-    [[ "$ZELLIJ_ZSH_DISABLE_VSCODE" = "true" && "$TERM_PROGRAM" = "vscode" ]]
+    _is_config_enabled DISABLE_VSCODE && [[ "$TERM_PROGRAM" = "vscode" ]]
 }
 
 if _is_zellij_cmd_available && ! _is_zellij_running && ! _is_vscode_disabled; then
+    _args=()
+    if _is_config_enabled DEBUG_MODE; then
+        _args+=("--debug")
+    fi
+
     function _zellij_autostart() {
-        exec zellij
+        exec zellij ${_args[@]}
     }
 
     autoload -Uz add-zsh-hook
